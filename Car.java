2341bb02fe42dfc635530e6343047e7125294203
Eric Murta <ericmurta@hotmail.com>
2017-10-22 23:59:48 -0300

    Added car actor

---
+++ Car.java
@@ -0,0 +1,71 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    /**
+     * Act - do whatever the Car wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public int Resistance;
+    private int _count;
+    public Car() {
+        Resistance = 500;
+        _count = 0;
+    }
+    
+    public void act() 
+    {
+        move();
+        checkColision();
+        checkResistance();
+    }
+    
+    private void move() {
+        String key = Greenfoot.getKey();
+        if (key == "left" && this.getX() > 266) {
+            this.setLocation(getX()-130, getY());            
+        } 
+        else if(key == "right" && this.getX() < 662) {
+            this.setLocation(getX()+130, getY());            
+        }
+        
+    }
+    
+    public void hitDamage(int _damage) {
+        Resistance -= _damage;
+    }
+    
+    public void checkColision() {
+       if(this.getX() < 266 || this.getX() > 662) {
+           _count+=1;
+           if(_count%10 == 0) {
+               hitDamage(_count);
+            }
+            
+        }
+    }
+    
+    public void gameOver() {
+        getWorld().removeObject(this);
+        Greenfoot.stop();
+    }
+    
+    public int getResistance() {
+        return Resistance;
+    }
+    
+    public void checkResistance() {
+        if (Resistance <= 0) {
+            gameOver();
+        }
+    }
+    
+ 
+        
+}
--- MyWorld.java
+++ MyWorld.java
@@ -13,9 +13,31 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    GreenfootSound backgroundMusic = new GreenfootSound("Top Gear - Las Vegas.mp3");
+
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
+        prepare();
+    }
+
+    public void started() {
+        backgroundMusic.setVolume(50);
+        backgroundMusic.playLoop();
+    }
+
+    public void stopped() {
+        backgroundMusic.pause();        
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Car car = new Car();
+        addObject(car,559,530);
     }
 }
